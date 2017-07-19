a40bf536d8f9f041c85308a46f6ca12c376ac534
Trevor Preston <trevorpreston4@gmail.com>
2017-07-19 14:45:37 -0400

    added working twilio

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 console.log('KrAzee K0d3rz are for realz')
+
+require('dotenv').config()
+
+const accountSid = process.env.TWILIO_ID;
+const authToken = process.env.TWILIO_TOKEN;
+console.log(`accountSid is ${accountSid}`)
+console.log(`token is ${authToken}`)
+
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'KrAzee K0d3rz be KrAzee',
+    to: '+12066011711',  // Text this number
+    from: '+13608420945' // From our Twilio number
+})
+.then((message) => console.log(message.sid));
