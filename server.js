131873eccabf6ee430fb279356b30dceae2a794f
Trevor Preston <trevorpreston4@gmail.com>
2017-07-20 13:27:57 -0400

    trev stuffs

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ console.log(`token is ${authToken}`)
 const twilio = require('twilio');
 const client = new twilio(accountSid, authToken);
 
+
+// wrap this in a function
 client.messages.create({
     body: 'KrAzee K0d3rz be KrAzee',
     to: '+12066011711',  // Text this number
     from: '+13608420945' // From our Twilio number
 })
 .then((message) => console.log(message.sid));
+
+
+// setup server
+// figure out how to detect sensor event
+
+// when sensor event fires, fire twilio function
