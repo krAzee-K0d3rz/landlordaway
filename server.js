60acedefa449d1a85d4c56561c77b50268dee9c7
shishkina <shishkina.kate@gmail.com>
2017-07-31 16:33:57 -0400

    add function to tast gpio

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 console.log('KrAzee K0d3rz are for realz');
 
+/*
+
+1.  decide on what library we’re going to use for reading the signal from the GPIO.
+2. test the library and get it to console.log something when the PIR signal is read.
+3. replace that console.log with the twilio function so that the twilio function fires with the signal event
+4.  buy champagne and celebrate our victory
+
+*/
+
 require('dotenv').config();
 
 const TWILIO_ID = process.env.TWILIO_ID;
@@ -11,15 +20,26 @@ console.log(`token is ${authToken}`)
 
 
 const twilio = require('twilio');
-const client = new twilio(accountSid, authToken);
 const express = require('express');
 const logger = require('morgan');
-const app = express();
+const gpio = require('pigpio').Gpio;
 
+const app = express();
+const echo = new Gpio(7, {mode: Gpio.INPUT, alert: true}),
+const client = new twilio(accountSid, authToken);
 app.use(logger('dev'));
 
+console.log(echo);
 
+
+
+function checkRead() {
+ echo.digitalRead() ? console.log('ALERT!!!!') : console.log('nothing')
+}
+
+setInterval(checkRead, 20)
 // wrap this in a function
+/*
 client.messages.create({
     body: 'KrAzee K0d3rz be KrAzee',
     to: USER_PHONE,  // Text this number
@@ -27,6 +47,7 @@ client.messages.create({
 })
 .then((message) => console.log(message.sid));
 
+*/
 
 /*app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port ${process.env.PORT}`);
